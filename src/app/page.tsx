55f1ebbc894b1bd48763a2318eac5b6b2c3a19b4
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-18 14:02:31 +0100

    feat: react-toastify library for response notification
    The updated code introduces the react-toastify library to handle response notifications, providing user feedback. The library is integrated within the handleSubmit function, providing status updates for 'pending', 'success', and 'error' cases. The changes aim to enhance user experience and improve response clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,30 @@
 
 import { FormEvent } from 'react';
 import { Form } from '@/components/form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const response = await fetch('/api/commit', {
-      method: 'POST',
-      body: formData,
-    });
-    console.log(response);
+    await toast.promise(
+      fetch('/api/commit', {
+        method: 'POST',
+        body: formData,
+      }),
+      {
+        pending: 'Committing...',
+        success: 'Successfully committed!',
+        error: 'Failed to commit.',
+      },
+    );
   }
 
   return (
     <main className="flex justify-center">
       <Form onSubmit={handleSubmit} />
+      <ToastContainer theme="light" position="top-center" />
     </main>
   );
 }
